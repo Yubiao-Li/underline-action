98c39fb25ed5da8f45b1acd42aa0864731d25c6f
biao <805650945@qq.com>
2022-12-13 20:20:16 +0800

    修复换行逻辑

--- index.js
+++ index.js
@@ -9,13 +9,7 @@ function findFirstBlockParent(dom) {
   return parent;
 }
 
-export function needWrap(dom1, dom2) {
-  if (!dom1 || !dom2) return false;
-  const parentBlockNode = findFirstBlockParent(dom1);
-  // dom1的第一个block父节点不是dom2的父节点，说明需要换行
-  if (parentBlockNode && !parentBlockNode.contains(dom2)) {
-    return true;
-  }
+function findBr(dom1, dom2) {
   // 判断两个节点之间是否有br节点
   const range = document.createRange();
   range.setStart(dom2, 0);
@@ -23,6 +17,16 @@ export function needWrap(dom1, dom2) {
   const div = document.createElement('div');
   div.appendChild(range.cloneContents());
   return div.getElementsByTagName('br').length !== 0;
+}
+
+export function needWrap(dom1, dom2) {
+  if (!dom1 || !dom2) return false;
+  const parentBlockNode = findFirstBlockParent(dom1);
+  // dom1的第一个block父节点不是dom2的父节点，说明需要换行
+  if (parentBlockNode && !parentBlockNode.contains(dom2)) {
+    return true;
+  }
+  return findBr(dom1, dom2) || findBr(dom2, dom1);
 }
 
 export function UnderlineAction(opt) {
