4a859825b5430d11b6ef268211772a943b92742b
cadeli <cadeli@tencent.com>
2023-05-30 10:39:45 +0800

    wrap underline half

---
+++ src/lib/core/highlightSpan.ts
@@ -0,0 +1,25 @@
+declare global {
+  interface HTMLElement {
+    _isHighlightSpan: boolean;
+  }
+}
+
+export function createHighlightSpan(props: any, tag?: string) {
+  const span = document.createElement(tag || 'span');
+  span._isHighlightSpan = true;
+  // span.textContent = content;
+  span.className = 'underline';
+  Object.keys(props).forEach(key => (span[key] = props[key]));
+  return span;
+}
+
+export function isHighlightSpan(dom: HTMLElement) {
+  return dom._isHighlightSpan;
+}
+
+export function findParentHighlightSpan(dom: Text) {
+  let ele = dom.parentElement;
+  do {
+    if (isHighlightSpan(ele)) return ele;
+  } while ((ele = ele.parentElement));
+}
